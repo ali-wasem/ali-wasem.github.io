922738b18f0ae8f023fc027f26825fce0ee10015
aliwaseem27 <ali.waseem27@gmail.com>
2024-09-08 21:27:38 +0300

    fix: inejction

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,6 +1,5 @@
 // Function to inject the modal into the page
-function injectModal() {
-  var modalHTML = `
+var modalHTML = `
     <div id="myModal" class="modal">
       <span class="close">&times;</span>
       <img class="modal-content" id="img01">
@@ -8,47 +7,37 @@ function injectModal() {
     </div>
   `;
 
-  // Inject the modal HTML into the body
-  document.body.insertAdjacentHTML("beforeend", modalHTML);
-}
+// Inject the modal HTML into the body
+document.body.insertAdjacentHTML("beforeend", modalHTML);
 
-// Function to initialize the modal functionality
-function initializeModal() {
-  // Get the modal
-  var modal = document.getElementById("myModal");
+// Get the modal
+var modal = document.getElementById("myModal");
 
-  // Get all images with the class 'modal-trigger'
-  var images = document.querySelectorAll(".modal-trigger");
-  var modalImg = document.getElementById("img01");
-  var captionText = document.getElementById("caption");
+// Get all images with the class 'modal-trigger'
+var images = document.querySelectorAll(".modal-trigger");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
 
-  // Loop through all selected images and add the click event
-  images.forEach(function (img) {
-    img.onclick = function () {
-      modal.style.display = "block";
-      modalImg.src = this.src;
-      captionText.innerHTML = this.alt;
-    };
-  });
+// Loop through all selected images and add the click event
+images.forEach(function (img) {
+  img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  };
+});
 
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function () {
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+modal.onclick = function (event) {
+  if (event.target == modal) {
     modal.style.display = "none";
-  };
-
-  // When the user clicks anywhere outside of the modal, close it
-  modal.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
-}
-
-// Call the function to inject the modal
-injectModal();
-
-// Call the function to initialize the modal functionality
-initializeModal();
+  }
+};
